71801faa32227772c16d721d08dce642849491e8
Jennifer Bain <jennifer.fleming.bain@gmail.com>
2021-01-08 17:19:21 -0700

    update airport data to include only operational airports

--- back-end/rawData/airport_carpentry.sh
+++ back-end/rawData/airport_carpentry.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 wget https://ourairports.com/data/airports.csv 
-awk -F, '$3 ~ /large_airport/' airports.csv > large_airports.csv
+awk -F, '$3 ~ /large_airport/ && $12 ~ /yes/' airports.csv > large_airports.csv
 rm airports.csv
