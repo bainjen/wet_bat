b06db79ae2a1bb32a6d0b60dc003f927bce5e5ac
Jennifer Bain <jennifer.fleming.bain@gmail.com>
2021-01-09 18:19:05 -0700

    successful query to db to get all customers

--- back-end/routes/router.js
+++ back-end/routes/router.js
@@ -3,12 +3,27 @@ const express = require("express");
 const router = express.Router();
 const cors = require("cors");
 const { Pool } = require("pg");
+
 const dbConfig = {
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
 };
 
+const corsOptions = {
+  origin: "http://localhost:3000",
+  optionsSuccessStatus: 200,
+};
+
 const pool = new Pool(dbConfig);
 
-pool.connect(console.log("connected to the db"));
+const getAllCustomers = async () => {
+  const client = await pool.connect(console.log("connected to the db"));
+  const query = "SELECT * FROM customers";
+  return await client
+    .query(query)
+    .then((resp) => resp.rows)
+    .catch((err) => `Error at getAllCustomers ${err}`);
+};
+
+getAllCustomers().then((resp) => console.log(resp));
