5aff7bc68a1c0bee9d9ca4534ad1d2e19aae1259
Jennifer Bain <jennifer.fleming.bain@gmail.com>
2021-01-10 10:41:35 -0700

    implement autocomplete component from MUI and useState to update airports on QuoteForm

--- front-end/src/components/QuoteForm.jsx
+++ front-end/src/components/QuoteForm.jsx
@@ -4,6 +4,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import MenuItem from "@material-ui/core/MenuItem";
+import Autocomplete from "@material-ui/lab/Autocomplete";
 import DateFnsUtils from "@date-io/date-fns";
 import {
   MuiPickersUtilsProvider,
@@ -23,7 +24,6 @@ const useStyles = makeStyles((theme) => ({
 const QuoteForm = () => {
   const { dataState } = useContext(QuoteContext);
   const { transportation, airports, loaded } = dataState;
-  console.log("transportation", transportation);
 
   const classes = useStyles();
   const today = new Date();
@@ -31,6 +31,8 @@ const QuoteForm = () => {
   tomorrow.setDate(tomorrow.getDate() + 1);
   const [departureDate, setDepartureDate] = useState(today);
   const [returnDate, setReturnDate] = useState(tomorrow);
+  const [depFlight, setDepFlight] = useState();
+  const [retFlight, setRetFlight] = useState();
   const [numPeople, setNumPeople] = useState(1);
   const [transportOption, setTransportOption] = useState("");
 
@@ -71,71 +73,107 @@ const QuoteForm = () => {
       </MenuItem>
     );
   });
-
+  console.log("depFlight", depFlight);
   return (
     <MuiPickersUtilsProvider utils={DateFnsUtils}>
-      <form className={classes.root} noValidate autoComplete="off">
-        <div>
-          <TextField
-            required
-            label="FROM"
-            InputLabelProps={{
-              shrink: true,
-            }}
-            variant="filled"
-          />
-          <TextField
-            required
-            label="DESTINATION"
-            InputLabelProps={{
-              shrink: true,
-            }}
-            variant="filled"
-          />
+      {loaded && (
+        <form className={classes.root} noValidate autoComplete="off">
+          <div>
+            <Autocomplete
+              value={depFlight}
+              onChange={(event, newValue) => {
+                if (newValue) {
+                  setDepFlight(newValue.id);
+                } else {
+                  setDepFlight(null);
+                }
+              }}
+              options={airports}
+              getOptionLabel={(option) =>
+                `${option.municipality} (${option.iata_code})`
+              }
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  required
+                  label="FROM"
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                  variant="filled"
+                />
+              )}
+            />
 
-          <KeyboardDatePicker
-            disableToolbar
-            variant="inline"
-            format="MM/dd/yyyy"
-            margin="normal"
-            id="dep-date-picker-dialogue"
-            label="Departure Date"
-            value={departureDate}
-            onChange={handleDeparture}
-            KeyboardButtonProps={{
-              "aria-label": "change date",
-            }}
-          />
+            <Autocomplete
+              value={retFlight}
+              onChange={(event, newValue) => {
+                if (newValue) {
+                  setRetFlight(newValue.id);
+                } else {
+                  setRetFlight(null);
+                }
+              }}
+              options={airports}
+              getOptionLabel={(option) =>
+                `${option.municipality} (${option.iata_code})`
+              }
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  required
+                  label="DESTINATION"
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                  variant="filled"
+                />
+              )}
+            />
 
-          <KeyboardDatePicker
-            disableToolbar
-            variant="inline"
-            format="MM/dd/yyyy"
-            margin="normal"
-            id="ret-date-picker-dialogue"
-            label="Departure Date"
-            value={returnDate}
-            onChange={handleReturn}
-            KeyboardButtonProps={{
-              "aria-label": "change date",
-            }}
-          />
+            <KeyboardDatePicker
+              disableToolbar
+              variant="inline"
+              format="MM/dd/yyyy"
+              margin="normal"
+              id="dep-date-picker-dialogue"
+              label="Departure Date"
+              value={departureDate}
+              onChange={handleDeparture}
+              KeyboardButtonProps={{
+                "aria-label": "change date",
+              }}
+            />
 
-          <TextField
-            required
-            select
-            label="PEOPLE"
-            type="number"
-            InputLabelProps={{
-              shrink: true,
-            }}
-            variant="filled"
-            defaultValue={1}
-            onChange={handlePeople}
-          >
-            {numOptions}
-          </TextField>
-          {loaded && (
+            <KeyboardDatePicker
+              disableToolbar
+              variant="inline"
+              format="MM/dd/yyyy"
+              margin="normal"
+              id="ret-date-picker-dialogue"
+              label="Departure Date"
+              value={returnDate}
+              onChange={handleReturn}
+              KeyboardButtonProps={{
+                "aria-label": "change date",
+              }}
+            />
+
+            <TextField
+              required
+              select
+              label="PEOPLE"
+              type="number"
+              InputLabelProps={{
+                shrink: true,
+              }}
+              variant="filled"
+              defaultValue={1}
+              onChange={handlePeople}
+            >
+              {numOptions}
+            </TextField>
+
             <TextField
               select
               label="TRANSPORTATION"
@@ -148,27 +186,28 @@ const QuoteForm = () => {
             >
               {transportOptions}
             </TextField>
-          )}
-          <TextField
-            required
-            label="NAME"
-            InputLabelProps={{
-              shrink: true,
-            }}
-            variant="filled"
-          />
-          <Button
-            className={classes.btn}
-            variant="contained"
-            // color="primary"
-            disableElevation
-            size="large"
-            onClick={sendQuote}
-          >
-            Create a quote
-          </Button>
-        </div>
-      </form>
+
+            <TextField
+              required
+              label="NAME"
+              InputLabelProps={{
+                shrink: true,
+              }}
+              variant="filled"
+            />
+            <Button
+              className={classes.btn}
+              variant="contained"
+              // color="primary"
+              disableElevation
+              size="large"
+              onClick={sendQuote}
+            >
+              Create a quote
+            </Button>
+          </div>
+        </form>
+      )}
     </MuiPickersUtilsProvider>
   );
 };
